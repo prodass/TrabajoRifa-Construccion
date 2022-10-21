8390892782c880dc284fb82ebfffeaeb201b83fe
Pablo <prodas@facultad.sanfrancisco.utn.edu.ar>
2022-10-21 12:10:05 -0300

    actualizacion de codigo

--- Construccion/FormRifa.cs
+++ Construccion/FormRifa.cs
@@ -24,42 +24,49 @@ namespace Construccion
             MessageBox.Show(mensajeCuerpo, mensajeTitulo, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void validarEntradas(string nombrePersona, string apellidoPersona, string numeroRifa)
+        private bool formatoDeEntradaEsValido(string nombrePersona, string apellidoPersona, string numeroRifa)
         {
             bool formatoNombreNoEsValido = !Regex.Match(nombrePersona, @"^[A-Za-z]{3,10}$|^[A-Za-z]{3,10}\s[A-Za-z]{3,10}$").Success;
             bool formatoApellidoNoEsValido = !Regex.Match(apellidoPersona, @"^[A-Za-z]{3,13}$|^[A-Za-z]{3,13}\s[A-Za-z]{3,13}$").Success;
             bool formatoNumeroNoEsValido = !Regex.Match(numeroRifa, @"^(?:0|[1-9]\d{0,2})$").Success;
+            bool formatoDeEntradaValido = true;
 
             if (formatoNombreNoEsValido)
             {
                 mostrarMensaje("El nombre ingresado no es valido!", "Error de formato");
                 tboxNombre.Clear();
+                formatoDeEntradaValido = false;
             }
             if (formatoApellidoNoEsValido)
             {
                 mostrarMensaje("El apellido ingresado no es valido!", "Error de formato");
                 tboxApellido.Clear();
+                formatoDeEntradaValido = false;
             }
             if (formatoNumeroNoEsValido)
             {
                 mostrarMensaje("El numero ingresado no es valido!", "Error de formato");
                 tboxNumero.Clear();
+                formatoDeEntradaValido = false;
             }
+
+            return formatoDeEntradaValido;
         }
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            validarEntradas(tboxNombre.Text, tboxApellido.Text, tboxNumero.Text);
-
-            if (Persona.numeroRifaEstaDisponible(tboxNumero.Text))
+            if(formatoDeEntradaEsValido(tboxNombre.Text, tboxApellido.Text, tboxNumero.Text))
             {
-                mostrarMensaje($"El numero {tboxNumero.Text} ha sido asignado a {tboxNombre.Text} {tboxApellido.Text}.", "Sistema de rifas");
+                if (Persona.numeroRifaEstaDisponible(tboxNumero.Text))
+                {
+                    mostrarMensaje($"El numero {tboxNumero.Text} ha sido asignado a {tboxNombre.Text} {tboxApellido.Text}.", "Sistema de rifas");
 
-                Persona.registrarPersona(tboxNombre.Text, tboxApellido.Text, tboxNumero.Text);
-            }
-            else
-            {
-                mostrarMensaje("El numero solicitado ya ha sido ingresado, por favor, ingrese otro!", "Sistema de rifas");
+                    Persona.registrarPersona(tboxNombre.Text, tboxApellido.Text, tboxNumero.Text);
+                }
+                else
+                {
+                    mostrarMensaje("El numero solicitado ya ha sido ingresado, por favor, ingrese otro!", "Sistema de rifas");
+                }
             }
         }
     }
